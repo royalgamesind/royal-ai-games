5472ae816b89bd9fe9e4a0fb3f8ffe22378cf6bf
royalgamesind <toogle357@gmail.com>
2026-02-14 04:28:04 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
 
     const response = await fetch(
       "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=
-      =" +
+      " +
       process.env.GEMINI_API_KEY,
       {
         method: "POST",
