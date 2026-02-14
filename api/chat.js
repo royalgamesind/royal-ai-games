29055f43e3156cbc8dffae89c3276aa23b541265
royalgamesind <toogle357@gmail.com>
2026-02-14 05:35:09 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -17,8 +17,8 @@ export default async function handler(req, res) {
     console.log("API KEY VALUE:", process.env.GEMINI_API_KEY);
     
     const { message } = req.body;
-    const response = await fetch(
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + process.env.GEMINI_API_KEY,
+   const response = await fetch(
+  "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=" + process.env.GEMINI_API_KEY,
   {
     method: "POST",
     headers: { "Content-Type": "application/json" },
